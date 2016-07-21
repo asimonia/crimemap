2d4d3cd6a12243b863479d103bd2939c6ef1ca55
asimonia <alex.simonian@gmail.com>
2016-07-21 15:58:16 -0500

    Commit

--- crimemap.py
+++ crimemap.py
@@ -1,43 +1,66 @@
-from dbhelper import DBHelper
 from flask import Flask
 from flask import render_template
 from flask import request
-
+import json
+import dateparser
+import datetime
+import string
+import dbconfig
+if dbconfig.test:
+    from mockdbhelper import MockDBHelper as DBHelper
+else:
+    from dbhelper import DBHelper
 
 
 app = Flask(__name__)
 DB = DBHelper()
 
-
-@app.route('/')
-def home():
-	try:
-		data = DB.get_all_inputs()
-	except Exception as e:
-		print(e)
-		data = None
-	return render_template("home.html", data=data)
+categories = ['mugging', 'break-in']
 
 
-@app.route('/add', methods=['POST'])
-def add():
-	try:
-		data = request.form.get("username")
-		DB.add_input(data)
-	except Exception as e:
-		print(e)
-	return home()
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 
-@app.route('/clear')
-def clear():
-	try:
-		DB.clear_all()
-	except Exception as e:
-		print(e)
-	return home()
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+
+@app.route("/")
+def home(error_message=None):
+    crimes = DB.get_all_crimes()
+    crimes = json.dumps(crimes)
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
+
+
+@app.route("/submitcrime", methods=['POST'])
+def submitcrime():
+    try:
+        error_message = None
+        category = request.form.get("category")
+        if category not in categories:
+            return home()
+
+        date = format_date(request.form.get("date"))
+        if not date:
+            return home("Invalid date. Please use yyyy-mm-dd format")
+        try:
+            latitude = float(request.form.get("latitude"))
+            longitude = float(request.form.get("longitude"))
+        except:
+            error_message = "Latitude and Longitude have incorrect format"
+            return home(error_message)
+        description = sanitize_string(request.form.get("description"))
+        DB.add_crime(category, date, latitude, longitude, description)
+        return home()
+    except Exception as e:
+        print e
 
 
 if __name__ == '__main__':
-	app.run(port=5000, debug=True)
-	
+    app.run(debug=True)
